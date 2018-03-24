dacc1ee2eed6fa749d7e34b7c3270e9b13bd0c95
Renzo Bautista <renzobautista2@gmail.com>
2018-03-24 15:46:29 -0700

    added flow-typed to gitignore

--- tests/app/actions/budget.test.js
+++ tests/app/actions/budget.test.js
@@ -3,8 +3,8 @@ import { SET_BUDGET_TOTAL, SetBudgetTotalAction} from '/app/actions/ActionTypes'
  
 describe('BudgetActions', () => {
   it('should create an action to set new budget total', () => {
-    const total : number = 35000
-    const expectedAction : SetBudgetTotalAction = {
+    const total: number = 35000
+    const expectedAction: SetBudgetTotalAction = {
       type: SET_BUDGET_TOTAL,
       total
     }
