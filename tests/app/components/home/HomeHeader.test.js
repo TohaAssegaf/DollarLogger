55e52cb19cb2dc1d3a3c07ca50201c71526f9ae5
Renzo Bautista <renzobautista2@gmail.com>
2018-04-08 23:31:58 -0700

    added payment info to home tests

--- tests/app/components/home/HomeHeader.test.js
+++ tests/app/components/home/HomeHeader.test.js
@@ -1,4 +1,5 @@
 import HomeHeader from '/app/components/home/HomeHeader'
+import MockDate from 'mockdate'
 import React from 'react';
 import configureMockStore from 'redux-mock-store'
 import renderer from 'react-test-renderer';
@@ -8,7 +9,17 @@ const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares)
 
 it('renders correctly', () => {
-  const store = mockStore({ budget: { total: 25000 }, payment: { payments: [] }})
+  MockDate.set(new Date(2018, 3, 8))
+  const store = mockStore({
+    budget: { total: 25000 },
+    payment: { payments: [
+      {
+        id: 1,
+        total: 1,
+        name: "Test payment",
+        date: new Date(2018, 3, 8),
+      }
+    ]}})
   const rendered = renderer.create(<HomeHeader store={store} />).toJSON();
   expect(rendered).toMatchSnapshot();
 });
--- tests/app/screens/home/Home.test.js
+++ tests/app/screens/home/Home.test.js
@@ -9,7 +9,16 @@ const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares)
 
 it('renders correctly', () => {
-  const store = mockStore({ budget: { total: 25000 }, payment: { payments: [] }})
+  const store = mockStore({
+    budget: { total: 25000 },
+    payment: { payments: [
+      {
+        id: 1,
+        total: 1,
+        name: "Test payment",
+        date: new Date(2018, 3, 8),
+      }
+    ]}})
   const rendered = renderer.create(
     <Provider store={store}><Home /></Provider>).toJSON();
   expect(rendered).toMatchSnapshot();
