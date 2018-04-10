1054b51f2b0a148d17b66ffa22244e40262a4af7
Renzo Bautista <renzobautista2@gmail.com>
2018-04-09 23:10:32 -0700

    added payment screen tests

---
+++ tests/app/screens/payment/UpdatePayment.test.js
@@ -0,0 +1,20 @@
+import UpdatePayment from '/app/screens/payment/UpdatePayment'
+import React from 'react';
+import renderer from 'react-test-renderer';
+import configureMockStore from 'redux-mock-store'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+
+it('renders correctly', () => {
+  const payment = { id: 1, total: 1000, name: "Test payment", date: new Date(4, 3, 2018) }
+  const store = mockStore({ payment: { payments: [payment] } })
+  const rendered = renderer.create(
+    <Provider store={store}>
+      <UpdatePayment navigation={{ state: { params: { payment } } }} />
+    </Provider>
+  ).toJSON();
+  expect(rendered).toMatchSnapshot();
+});
