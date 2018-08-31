7d837c528d991bc07cae93360a8301af1c36e468
Renzo Bautista <renzobautista2@gmail.com>
2018-08-30 21:05:32 -0700

    installed react-native-fbsdk

--- android/app/src/main/java/com/dollarlogger/MainApplication.java
+++ android/app/src/main/java/com/dollarlogger/MainApplication.java
@@ -3,6 +3,7 @@ package com.dollarlogger;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FBSDKPackage(),
             new ReactNativeConfigPackage(),
             new RNFirebasePackage(),
             new VectorIconsPackage()
