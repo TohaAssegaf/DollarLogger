b17fd14b7782d7901c0b7fa9d64c548bf9b18415
Renzo Bautista <renzobautista2@gmail.com>
2018-09-08 17:11:58 -0700

    Create FBAuthStrategy to login with Facebook.

---
+++ app/lib/auth/FBAuthStrategy.js
@@ -0,0 +1,19 @@
+import { AccessToken, LoginManager } from 'react-native-fbsdk'
+import firebase from 'react-native-firebase'
+
+export const login = () => {
+  LoginManager.logInWithReadPermissions(['public_profile', 'email'])
+    .then(result -> {
+      if (result.isCancelled) {
+        throw new Error('User cancelled request.')
+      }
+      console.log(`Login success with permissions: ${result.grantedPermissions.toString()}`)
+      return AccessToken.getCurrentAccessToken()
+    }).then(data -> {
+      if (!data) {
+        throw new Error('Access token could not be obtained.')
+      }
+      const credential = firebase.auth.FacebookAuthProvider.credential(data.accessToken)
+      return firebase.auth().signInAndRetrieveDataWithCredential(credential)
+    })
+}
