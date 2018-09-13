91b0fb9f253f30c105df59633c8d8a2af58dc129
Renzo Bautista <renzobautista2@gmail.com>
2018-09-13 00:45:58 -0700

    Completed basic sign-in flow, functional on android.

--- app/lib/auth/FBAuth.js
+++ app/lib/auth/FBAuth.js
@@ -5,18 +5,15 @@ export const login = (error, result) => {
   if (error) {
     throw new Error('Login failed with error: ' + error.message)
   }
-  LoginManager.logInWithReadPermissions(['public_profile', 'email'])
-    .then(result => {
-      if (result.isCancelled) {
-        throw new Error('User cancelled request.')
-      }
-      console.log(`Login success with permissions: ${result.grantedPermissions.toString()}`)
-      return AccessToken.getCurrentAccessToken()
-    }).then(data => {
-      if (!data) {
-        throw new Error('Access token could not be obtained.')
-      }
-      const credential = firebase.auth.FacebookAuthProvider.credential(data.accessToken)
-      return firebase.auth().signInAndRetrieveDataWithCredential(credential)
-    })
+  if (result.isCancelled) {
+    throw new Error('User cancelled request.')
+  }
+  console.log(`Login success with permissions: ${result.grantedPermissions.toString()}`)
+  return AccessToken.getCurrentAccessToken().then(data => {
+    if (!data) {
+      throw new Error('Access token could not be obtained.')
+    }
+    const credential = firebase.auth.FacebookAuthProvider.credential(data.accessToken)
+    return firebase.auth().signInAndRetrieveDataWithCredential(credential)
+  })
 }
