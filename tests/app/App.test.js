3ab3a9a2dd448279c96e90d41440061330663fb8
Renzo Bautista <renzobautista2@gmail.com>
2018-09-21 17:02:20 -0700

    added firebase mock and jest config for it

--- tests/app/App.test.js
+++ tests/app/App.test.js
@@ -2,6 +2,20 @@ import React from 'react';
 import App from '~/app/App';
 
 import renderer from 'react-test-renderer';
+//import mocked RNFirebase
+import RNFirebase from 'react-native-firebase'
+
+//reset mocks
+beforeEach(() => {
+  RNFirebase.reset()
+})
+
+//set mocked data
+const ref = RNFirebase.firebase.database().ref('users/')
+ref.data = users
+
+//verify listener called
+expect(ref.on.mock.calls.length).toBe(1)
 
 it('renders without crashing', () => {
   const rendered = renderer.create( <App /> ).toJSON();
