bd256987c687aacfdf31b1c983e7042dd5d36182
Renzo Bautista <renzobautista2@gmail.com>
2018-09-23 14:08:35 -0700

    added flow/comment changes to FirebaseUtils. */

--- app/lib/database/FirebaseUtils.js
+++ app/lib/database/FirebaseUtils.js
@@ -3,12 +3,15 @@ import firebase from 'react-native-firebase'
 const PAYMENTS_KEY = 'payments'
 const VALUE_EVENT_TYPE = 'value'
 
-export function fetchPayments(successCallback) {
-  getPaymentsRef().once(VALUE_EVENT_TYPE, snapshot => successCallback(getPaymentsListFromSnapshot))
+/** Fetch payments from firebase, then run given callback. */
+export function fetchPayments(successCallback: Array<Payment> => void) {
+  getPaymentsRef().once(
+    VALUE_EVENT_TYPE, snapshot => successCallback(getPaymentsListFromSnapshot(snapshot)))
 }
 
-function getPaymentsListFromSnapshot(snapshot) {
-  let payments = []
+/** Given the payments snapshot, convert it to a list of Payments. */
+function getPaymentsListFromSnapshot(snapshot): Array<Payment> {
+  let payments: Array<Payment> = []
 
   // Add each child snapshot to payments list.
   snapshot.forEach(child => payments.push(child.toJSON()))
@@ -19,6 +22,7 @@ function getPaymentsListFromSnapshot(snapshot) {
 function getUserRef() {
   return firebase.database.ref(firebase.auth.currentUser.uid)
 }
+
 function getPaymentsRef() {
   return getUserRef().child(PAYMENTS)
 }
