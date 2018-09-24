443f6bd1c4a5d7a79c7e01d2abeb90c11a9db6f4
Renzo Bautista <renzobautista2@gmail.com>
2018-09-23 20:57:28 -0700

    updated login screen test with redux store

--- tests/app/screens/login/Login.test.js
+++ tests/app/screens/login/Login.test.js
@@ -1,8 +1,15 @@
 import Login from '~/app/screens/login/Login'
 import React from 'react';
 import renderer from 'react-test-renderer';
+import configureMockStore from 'redux-mock-store'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
 
 it('renders correctly', () => {
-  const rendered = renderer.create(<Login />).toJSON();
+  const store = mockStore({})
+  const rendered = renderer.create(<Provider store={store}><Login /></Provider>).toJSON();
   expect(rendered).toMatchSnapshot();
 });
