18a0222d2b8bfac7e67fccdac517721a37835215
Renzo Bautista <renzobautista2@gmail.com>
2018-09-25 00:18:13 -0700

    added FirebaseUtils test for single payment push

--- tests/app/lib/database/FirebaseUtils.test.js
+++ tests/app/lib/database/FirebaseUtils.test.js
@@ -22,3 +22,32 @@ it('pushes payment to mock firebase store and retrieves it', async () => {
 
   expect(payments).toEqual([payment])
 })
+
+it('pushes single payment to mock firebase store and retrieves it', async () => {
+  const payment = new PaymentBuilder()
+    .setTotal(10000)
+    .setName("Test payment")
+    .setDate(new Date(2018, 4, 2))
+    .build()
+
+  await FirebaseUtils.pushPayment(payment)
+
+  const payments = await FirebaseUtils.fetchPayments()
+
+  expect(payments).toEqual([payment])
+})
+
+it('updates single payment to mock firebase store and retrieves it', async () => {
+  const payment = new PaymentBuilder()
+    .setTotal(10000)
+    .setName("Test payment")
+    .setDate(new Date(2018, 4, 2))
+    .build()
+  await FirebaseUtils.pushPayment(payment)
+  const updatedPayment = new PaymentBuilder(payment).setTotal(20000).build()
+  await FirebaseUtils.pushPayment(updatedPayment)
+
+  const payments = await FirebaseUtils.fetchPayments()
+
+  expect(payments).toEqual([updatedPayment])
+})
