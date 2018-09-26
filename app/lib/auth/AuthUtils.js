28aeec93dcf2d138227a8f20bb9437bfd72fd227
Renzo Bautista <renzobautista2@gmail.com>
2018-09-26 01:55:12 -0700

    Add AuthUtils.logout method to log user out of firebase and all auth strategies.

--- app/lib/auth/AuthUtils.js
+++ app/lib/auth/AuthUtils.js
@@ -1,5 +1,15 @@
 import firebase from 'react-native-firebase'
+import * as FBAuth from './FBAuth'
+
+const AUTH_STRATEGIES = [FBAuth]
 
 export function isLoggedIn() {
   return firebase.auth().currentUser !== null
+}
+
+export function logout() {
+  firebase.auth().signOut()
+  for (const strategy of AUTH_STRATEGIES) {
+    strategy.logout()
+  }
 }
