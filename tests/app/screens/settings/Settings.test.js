007364e61eb095ce79347b45bc96b3961768eaf6
Renzo Bautista <renzobautista2@gmail.com>
2018-09-26 23:23:45 -0700

    added tests for clear local payments feature

--- tests/app/screens/settings/Settings.test.js
+++ tests/app/screens/settings/Settings.test.js
@@ -2,16 +2,23 @@ import * as AuthUtils from '~/app/lib/auth/AuthUtils'
 import Settings from '~/app/screens/settings/Settings'
 import React from 'react';
 import renderer from 'react-test-renderer';
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
 
 it('renders correctly', () => {
-  const component = <Settings />
+  const store = mockStore({})
+  const component = <Settings store={store} />
   const loggedInRender = renderer.create(component).toJSON()
   expect(loggedInRender).toMatchSnapshot()
 
 });
 
 it('triggers force update listener on logout', () => {
-  const component = <Settings />
+  const store = mockStore({})
+  const component = <Settings store={store} />
   AuthUtils.logout()
 
   const loggedOutRender = renderer.create(component).toJSON()
