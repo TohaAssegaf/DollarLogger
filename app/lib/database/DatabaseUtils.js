856b08b5ac1909781fea6fac5acfe07c4a6a7b52
Renzo Bautista <renzobautista2@gmail.com>
2018-09-28 00:27:23 -0700

    prioritize more recently updated local payments over db payments

--- app/lib/database/DatabaseUtils.js
+++ app/lib/database/DatabaseUtils.js
@@ -5,20 +5,40 @@ export function syncPayments(
   // Initialize synced payments list.
   let syncedPayments: Array<Payment> = []
 
-  // Add all DB payments (source of truth, overriding any local edits for now).
+  // Create maps from id to payment.
+  let localPaymentsMap = {}
+  for (const localPayment of localPayments) {
+    localPaymentsMap[localPayment.id] = localPayment
+  }
+  let dbPaymentsMap = {}
   for (const dbPayment of dbPayments) {
-    syncedPayments.push(dbPayment)
+    dbPaymentsMap[dbPayment.id] = dbPayment
+  }
+
+  // Add all DB payments, deferring to local payments if they were updated more recently.
+  for (let dbPayment of dbPayments) {
+    // Not all DB payments have updateTimestamp yet
+    // TODO(renzobautista): Clean up when all payments are migrated.
+    const dbUpdateTimestamp = dbPayment.updateTimestamp ? dbPayment.updateTimestamp : dbPayment.id
+    if (dbPayment.id in localPaymentsMap
+        && localPaymentsMap[dbPayment.id].updateTimestamp > dbUpdateTimestamp) {
+      syncedPayments.push(localPaymentsMap[dbPayment.id])
+    } else {
+      if (!('updateTimestamp' in dbPayment)) {
+        dbPayment.updateTimestamp = dbUpdateTimestamp
+      }
+      syncedPayments.push(dbPayment)
+    }
   }
 
   // Add all local payments that have not been synced for any reason.
-  const dbIds = dbPayments.map(payment => payment.id)
   for (const localPayment of localPayments) {
-    if (!dbIds.includes(localPayment.id)) {
+    if (!(localPayment.id in dbPaymentsMap)) {
       syncedPayments.push(localPayment)
     }
   }
 
-  // If a payment was deleted locally or in DB, delete it.
+  // Regardless of if a payment was deleted locally or in DB, delete it.
   let deletedIds = getDeletedIds(localPayments, dbPayments)
   return syncedPayments.map(
     syncedPayment =>
--- tests/app/lib/database/DatabaseUtils.test.js
+++ tests/app/lib/database/DatabaseUtils.test.js
@@ -1,6 +1,7 @@
 import PaymentBuilder from '~/app/lib/PaymentBuilder'
 import * as PaymentUtils from '~/app/lib/PaymentUtils'
 import * as DatabaseUtils from '~/app/lib/database/DatabaseUtils'
+import MockDate from 'mockdate'
 
 it('syncs local payments if none exist in database', () => {
   const localPayment: Payment = new PaymentBuilder()
@@ -54,4 +55,49 @@ it('sets unsynced deleted DB payment to be deleted', () => {
   const payments = DatabaseUtils.syncPayments([localPayment], [deletedDbPayment])
 
   expect(payments).toEqual([deletedDbPayment])
+})
+
+it('prioritizes unsynced updated local payment over DB payment', () => {
+  MockDate.set(new Date(2018, 3, 8))
+  const dbPayment: Payment = new PaymentBuilder()
+    .setTotal(10000)
+    .setDate(new Date(4, 4, 2018))
+    .setName("Test payment")
+    .build()
+  MockDate.set(new Date(2018, 3, 9))
+  const localPayment: Payment = new PaymentBuilder(dbPayment).setTotal(20000).build()
+
+  const payments = DatabaseUtils.syncPayments([localPayment], [dbPayment])
+
+  expect(payments).toEqual([localPayment])
+})
+
+it('prioritizes unsynced updated DB payment over local payment', () => {
+  MockDate.set(new Date(2018, 3, 8))
+  const localPayment: Payment = new PaymentBuilder()
+    .setTotal(10000)
+    .setDate(new Date(4, 4, 2018))
+    .setName("Test payment")
+    .build()
+  MockDate.set(new Date(2018, 3, 9))
+  const dbPayment: Payment = new PaymentBuilder(localPayment).setTotal(20000).build()
+
+  const payments = DatabaseUtils.syncPayments([localPayment], [dbPayment])
+
+  expect(payments).toEqual([dbPayment])
+})
+
+it('adds updateTimestamp field to DB payment if none exists', () => {
+  MockDate.set(new Date(2018, 3, 9))
+  const expectedDbPayment: Payment = new PaymentBuilder()
+    .setTotal(10000)
+    .setDate(new Date(4, 4, 2018))
+    .setName("Test payment")
+    .build()
+  let initialDbPayment: Payment = Object.assign({}, expectedDbPayment)
+  delete initialDbPayment['updateTimestamp']
+
+  const payments = DatabaseUtils.syncPayments([], [initialDbPayment])
+
+  expect(payments).toEqual([expectedDbPayment])
 })
