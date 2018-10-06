b15509e2410be6b44e40820959d6d912d4a17751
Renzo Bautista <renzobautista2@gmail.com>
2018-10-05 18:59:59 -0700

    add tests for HomeWeekDetails and PaymentUtils

--- tests/app/lib/PaymentUtils.test.js
+++ tests/app/lib/PaymentUtils.test.js
@@ -105,6 +105,110 @@ it('filters current week payment contributions correctly', () => {
   expect(includedPaymentContributions [1]).toEqual(includedPaymentContribution2)
 })
 
+it('filters week payments for date correctly', () => {
+  const date = new Date(2018, 3, 8)
+  const expiredPayment = {
+    id: 1,
+    total: 1,
+    name: "Expired test payment",
+    date: new Date(2018, 3, 1),
+    paymentContributions: [
+      {
+        displayName: "Expired test payment",
+        total: 1,
+        date: new Date(2018, 3, 1)
+      },
+    ],
+  }
+  const includedPayment1 = {
+    id: 2,
+    total: 2,
+    name: "Included test payment",
+    date: new Date(2018, 3, 2),
+    paymentContributions: [
+      {
+        displayName: "Included test payment",
+        total: 2,
+        date: new Date(2018, 3, 2)
+      },
+    ],
+  }
+  const includedPayment2 = {
+    id: 3,
+    total: 3,
+    name: "Included test payment",
+    date: new Date(2018, 3, 8),
+    paymentContributions: [
+      {
+        displayName: "Included test payment",
+        total: 3,
+        date: new Date(2018, 3, 8)
+      },
+    ],
+  }
+  const payments = [expiredPayment, includedPayment1, includedPayment2]
+
+  const filteredPayments = PaymentUtils.filterPaymentsForWeek(payments, date)
+
+  expect(filteredPayments).toHaveLength(2)
+  expect(filteredPayments[0]).toEqual(includedPayment1)
+  expect(filteredPayments[1]).toEqual(includedPayment2)
+})
+
+it('filters week payment contributions for date correctly', () => {
+  const date = new Date(2018, 3, 8)
+
+  const expiredPaymentContribution = {
+    displayName: "Expired test payment",
+    total: 1,
+    date: new Date(2018, 3, 1),
+  }
+  const includedPaymentContribution1 = {
+    displayName: "Included test payment",
+    total: 2,
+    date: new Date(2018, 3, 2)
+  }
+  const includedPaymentContribution2 = {
+    displayName: "Included test payment",
+    total: 3,
+    date: new Date(2018, 3, 8)
+  }
+  const expiredPayment = {
+    id: 1,
+    total: 1,
+    name: "Expired test payment",
+    date: new Date(2018, 3, 1),
+    paymentContributions: [expiredPaymentContribution],
+  }
+  const includedPayment1 = {
+    id: 2,
+    total: 2,
+    name: "Included test payment",
+    date: new Date(2018, 3, 2),
+    paymentContributions: [includedPaymentContribution1],
+  }
+  const includedPayment2 = {
+    id: 3,
+    total: 3,
+    name: "Included test payment",
+    date: new Date(2018, 3, 8),
+    paymentContributions: [includedPaymentContribution2],
+  }
+  const paymentContributions = [
+    expiredPaymentContribution,
+    includedPaymentContribution1,
+    includedPaymentContribution2,
+  ]
+  const payments = [expiredPayment, includedPayment1, includedPayment2]
+
+  const includedPaymentContributions = PaymentUtils.filterPaymentContributionsForWeek(
+    payments, date)
+
+  expect(includedPaymentContributions).toHaveLength(2)
+  expect(includedPaymentContributions[0]).toEqual(includedPaymentContribution1)
+  expect(includedPaymentContributions [1]).toEqual(includedPaymentContribution2)
+})
+
 it('calculates total spend correctly', () => {
   const total1 = 2
   const total2 = 3
