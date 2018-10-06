0977eca35bd43ee1f2d95aa664146380297ce136
Renzo Bautista <renzobautista2@gmail.com>
2018-10-05 18:46:29 -0700

    Cleaned up swiper front-end, setting title to starting week date or message for last/this/next week

--- app/screens/home/Home.js
+++ app/screens/home/Home.js
@@ -7,28 +7,37 @@ import HomeActionButton from '~/app/components/home/HomeActionButton'
 import { HEADER_BACKGROUND_COLOR, HEADER_TEXT_COLOR} from '~/app/config/colors'
 import * as DateUtils from '~/app/lib/DateUtils'
 import * as PaymentUtils from '~/app/lib/PaymentUtils'
-import React from 'react';
+import moment from 'moment'
+import React from 'react'
 import { Button, Text, TouchableOpacity, View } from 'react-native'
 import Swiper from 'react-native-swiper';
-import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/Ionicons'
 import { connect } from 'react-redux'
 
 const ONE_MINUTE = 60000
+const THIS_WEEK = 'This week'
+const LAST_WEEK = 'Last week'
+const NEXT_WEEK = 'Next week'
 
 class Home extends React.Component {
-  static navigationOptions = ({ navigation }) => ({
-    title: 'Home',
-    headerStyle: {
-      backgroundColor: HEADER_BACKGROUND_COLOR,
-    },
-    headerTintColor: HEADER_TEXT_COLOR,
-    headerRight:
-      <TouchableOpacity
-          style={styles.settingsButton}
-          onPress={() => navigation.navigate(Routes.SETTINGS)}>
-        <Icon name="md-settings" size={25} color="white" />
-      </TouchableOpacity>,
-  })
+  static navigationOptions = ({ navigation }) => {
+    const title = navigation.state.params && navigation.state.params.title
+      ? navigation.state.params.title
+      : 'This week'
+    return {
+      title,
+      headerStyle: {
+        backgroundColor: HEADER_BACKGROUND_COLOR,
+      },
+      headerTintColor: HEADER_TEXT_COLOR,
+      headerRight:
+        <TouchableOpacity
+            style={styles.settingsButton}
+            onPress={() => navigation.navigate(Routes.SETTINGS)}>
+          <Icon name="md-settings" size={25} color="white" />
+        </TouchableOpacity>,
+    }
+  }
 
   componentDidMount() {
     this.intervalId = setInterval(
@@ -50,28 +59,41 @@ class Home extends React.Component {
     this.clearInterval(this.intervalId)
   }
 
-  getPaymentWeeks() {
-    let dates = []
-    let times = new Set()
-    let weekStarts = this.props.paymentContributions.map(
-      paymentContribution => DateUtils.getLastMonday(paymentContribution.date))
-    for (const date of weekStarts) {
-      if (!times.has(date.getTime())) {
-        times.add(date.getTime())
-        dates.push(date)
-      }
-    }
-    return dates.sort()
+  getCurrentWeekIndex() {
+    return this.props.paymentWeeks.map(
+      date => date.getTime()).indexOf(DateUtils.getLastMonday(new Date()).getTime())
   }
 
   getHomeWeekDetailsList() {
-    return this.getPaymentWeeks().map(date => <HomeWeekDetails date={date} />)
+    return this.props.paymentWeeks.map(date => <HomeWeekDetails date={date} />)
+  }
+
+  changeNavigationTitle(title: string) {
+    this.props.navigation.setParams({ title })
+  }
+
+  onSwiperIndexChanged(index: number) {
+    let title = moment(this.props.paymentWeeks[index]).format('MMMM Do YYYY')
+    if (index === this.getCurrentWeekIndex()) {
+      title = THIS_WEEK
+    }
+    if (index === this.getCurrentWeekIndex() - 1) {
+      title = LAST_WEEK
+    }
+    if (index === this.getCurrentWeekIndex() + 1) {
+      title = NEXT_WEEK
+    }
+    this.changeNavigationTitle(title)
   }
 
   render() {
     return (
       <View style={styles.screen}>
-        <Swiper>
+        <Swiper
+            loop={false}
+            index={this.getCurrentWeekIndex()}
+            showsPagination={false}
+            onIndexChanged={index => this.onSwiperIndexChanged(index)}>
           {this.getHomeWeekDetailsList()}
         </Swiper>
         <HomeActionButton
@@ -83,9 +105,25 @@ class Home extends React.Component {
   }
 }
 
+function getPaymentWeeks(paymentContributions: Array<PaymentContribution>): Array<Date> {
+  let dates = []
+  let times = new Set()
+  let weekStarts = paymentContributions.map(
+    paymentContribution => DateUtils.getLastMonday(paymentContribution.date))
+  for (const date of weekStarts) {
+    if (!times.has(date.getTime())) {
+      times.add(date.getTime())
+      dates.push(date)
+    }
+  }
+  return dates.sort((a, b) => a - b)
+}
+
 const mapStateToProps = state => {
+  const paymentContributions = PaymentUtils.getSortedPaymentContributions(state.payment.payments)
   return {
-    paymentContributions: PaymentUtils.getSortedPaymentContributions(state.payment.payments),
+    paymentContributions: paymentContributions,
+    paymentWeeks: getPaymentWeeks(paymentContributions)
   }
 }
 
